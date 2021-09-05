43f11ee6ed25b6401b6da4a15f4b0b0f077880a3
coleloui <coleloui18@gmail.com>
2021-09-05 13:49:20 -0700

    getting recipe info

--- script.js
+++ script.js
@@ -7,18 +7,28 @@ const xiv = new XIVAPI({
 let itemID
 
 const itemInfo = () => {
-        xiv.search('Slithersand').then((res) => {
+        xiv.search('Rarefied Lemonade').then((res) => {
             itemID = res.Results[0].ID
             items(itemID)
         }).catch((err) =>{
             console.log(err)
         })
     }
-const items = (id) => {
-    xiv.data.get('item', id).then((res) => {
-        console.log(`Name`, res.Name)
-        console.log(`Type`, res.ItemSearchCategory.Name)
-    })
-}
+    const items = (id) => {
+        xiv.data.get('item', id).then((res) => {
+            const recipeId = res.Recipes[0].ID
+            recipe(recipeId)
+            // console.log(`Name`, res.Name)
+            // console.log(`Type`, res.ItemSearchCategory.Name)
+        })
+    }
+
+    const recipe = (id) => {
+        xiv.data.get('Recipe', id).then((res) => {
+            console.log(res)
+            // console.log(`Name`, res.Name)
+            // console.log(`Type`, res.ItemSearchCategory.Name)
+        })
+    }
 
 itemInfo()
