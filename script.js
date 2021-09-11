348e349a2be914041e8c711d3428f4a464b924f0
coleloui <coleloui18@gmail.com>
2021-09-11 12:59:34 -0700

    item and recipe

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const { lookup } = require('dns');
 const inquirer = require('inquirer');
+const { recipe } = require('./util/API');
 const API = require('./util/API');
 
 const item = [
@@ -9,15 +11,52 @@ const item = [
   },
 ];
 
+const options = [
+  {
+    type: 'list',
+    message: 'what would you like to do next',
+    choices: ['Item Info', 'Recipe Info', 'End'],
+    name: 'options',
+  },
+];
+
+// const loop = async (thingy) => {
+//   console.log(`thingy`, thingy);
+
+// };
+
 const test = async (info) => {
-  console.log(`info`, info);
+  const a = await API.items(info.Results[0].ID);
+  console.log(`a`, a);
+};
+
+const rInfo = async (info) => {
+  const a = await API.items(info.Results[0].ID);
+  //   console.log(`a`, a.Recipes);
+  const b = await API.recipe(a.Recipes[0].ID);
+  loop(b);
 };
 
 const question = async () => {
-  const answer = await inquirer.prompt(item);
-  const { thing } = answer;
-  const ping = await API.itemInfo(thing);
-  test(ping);
+  try {
+    const answer = await inquirer.prompt(item);
+    const ping = await API.itemInfo(answer.thing);
+    const next = await inquirer.prompt(options);
+    switch (next.options) {
+      case 'Item Info':
+        test(ping);
+        break;
+      case 'Recipe Info':
+        rInfo(ping);
+        break;
+      case 'End':
+        break;
+      default:
+        throw new Error('end');
+    }
+  } catch (err) {
+    console.log(`err`, err);
+  }
 };
 
 question();
--- util/API.js
+++ util/API.js
@@ -7,27 +7,15 @@ const xiv = new XIVAPI({
 
 const API = {
   recipe(id) {
-    xiv.data.get('Recipe', id).then(
-      (res) => res
-      // console.log(`Name`, res.Name)
-      // console.log(`Type`, res.ItemSearchCategory.Name)
-    );
+    return xiv.data.get('Recipe', id).catch((err) => console.log(`err`, err));
   },
 
   items(id) {
-    xiv.data.get('item', id).then((res) => {
-      //   const recipeId = res.Recipes[0].ID;
-      const recipeId = res;
-      console.log(recipeId);
-      // console.log(`Name`, res.Name)
-      // console.log(`Type`, res.ItemSearchCategory.Name)
-    });
+    return xiv.data.get('item', id).catch((err) => console.log(err));
   },
 
   itemInfo(item) {
-    return xiv.search(item).catch((err) => {
-      console.log(err);
-    });
+    return xiv.search(item).catch((err) => console.log(err));
   },
 };
 
